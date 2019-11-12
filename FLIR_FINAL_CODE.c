4babd31bba0e7efd67ccc8e5fd1d647989ad2b32
burche1 <burche.t.d@gmail.com>
2019-11-12 20:01:40 -0300

    Add files via upload

---
+++ FLIR_FINAL_CODE.c
@@ -0,0 +1,260 @@
+/*
+ * Original code built to Raspberry Pi 3+ and Flir Lepton 3
+
+*******************************************
+October 2019
+Made by Alexandre Gustavo Burche
+*/
+
+
+#include "leptonSDKEmb32PUB/LEPTON_SDK.h"
+#include "leptonSDKEmb32PUB/LEPTON_SYS.h"
+#include "leptonSDKEmb32PUB/LEPTON_VID.h"
+#include "leptonSDKEmb32PUB/LEPTON_AGC.h"
+#include "leptonSDKEmb32PUB/LEPTON_Types.h"
+
+#include <iostream>
+#include <wiringPi.h>
+
+
+#define NUM 1
+
+
+LEP_UINT8   deviceAddress = 0x2A;
+LEP_UINT16  regPowerOnOff = 0x0000;
+LEP_UINT16  regStatus = 0x0002;
+LEP_UINT16  regComID = 0x0004;
+LEP_UINT16  regDataLenght = 0x0006;
+LEP_UINT16  regData0 = 0x0008;
+LEP_UINT16  readDataPtr[NUM];
+LEP_UINT16  writeDataPtr[NUM];
+LEP_UINT16  wordsToRead = NUM;
+LEP_UINT16  wordsToWrite = NUM;
+LEP_UINT16  numWordsRead;
+LEP_UINT16  numWordsWritten;
+LEP_UINT16  status;
+LEP_UINT16	commandID;
+
+LEP_CAMERA_PORT_DESC_T _port;
+LEP_SYS_STATUS_E sysStatus;
+
+//LEP_UINT16  writeDataPtr[NUM] = ;
+//LEP_UINT16  commandID = ;
+
+
+/*
+    never used...  
+    just datasheet reference
+*/
+void LeptonTransmission(writeDataPtr)
+{
+	//1. Polling the status register until camera is ready for a new command (BUSY bit clear)
+	while(DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status) != 0);
+	//2. Polling the SYS FFC Status command (see 4.5.18) to verify the system is ready to process a new command (System Ready returned).
+	while(sysStatus == LEP_SYS_STATUS_BUSY)
+	{
+		LEP_GetSysFFCStatus(&_port,&sysStatus);
+	}
+	//3. Writing data to send to the camera if required into the DATA Registers or block Data buffer.
+	DEV_I2C_MasterWriteData(1, deviceAddress, regData0, writeDataPtr, wordsToWrite, &numWordsWritten, &status);
+	//4. Writing the number of data words written (16-bit data words) to the Data Length Register.
+	DEV_I2C_MasterWriteData(1, deviceAddress, regDataLenght, NUM, wordsToWrite, &numWordsWritten, &status);
+	//5. Writing the desired command ID to the Command Register.
+	DEV_I2C_MasterWriteData(1, deviceAddress, regComID, commandID, wordsToWrite, &numWordsWritten, &status);
+	//6. Polling the Status Register to determine when the command is completed (busy bit cleared).
+	DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	while((readDataPtr & 0x0001) != 0x0000)
+	{
+		DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	}
+	//7. Read the success code from the status register.
+	DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	//8. Retrieve any responses as required from the Data registers or block Data buffer.
+	DEV_I2C_MasterReadData(1, deviceAddress, regData0, readDataPtr, wordsToRead, &numWordsRead, &status);
+}
+
+/*  
+    Function to read DATA0 register
+    Return: data read
+    Inputs: 
+        -
+        -
+*/
+LEP_RESULT LeptonGet()
+{
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    DEV_I2C_MasterWriteData(1, deviceAddress, regDataLenght, NUM, wordsToWrite, &numWordsWritten, &status);
+    DEV_I2C_MasterWriteData(1, deviceAddress, regComID, commandID, wordsToWrite, &numWordsWritten, &status);
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    //Possivelmente na linha abaixo, devo fornecer o resultado da leitura a uma função de tratativa de erro
+    //Inserir parte do código para tratamento de erro... se resultado for OK sigo adiante, se for um erro processo o erro e saio da função.
+    DEV_I2C_MasterReadData(1, deviceAddress, regData0, readDataPtr, wordsToRead, &numWordsRead, &status);
+
+    return readDataPtr;
+}
+
+/*  
+    Function to write Command ID register
+    Return: none
+    Inputs: 
+        -
+        -
+*/
+void LeptonSet()
+{
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    DEV_I2C_MasterWriteData(1, deviceAddress, regData0, writeDataPtr, wordsToWrite, &numWordsWritten, &status);
+    DEV_I2C_MasterWriteData(1, deviceAddress, regDataLenght, NUM, wordsToWrite, &numWordsWritten, &status);
+    DEV_I2C_MasterWriteData(1, deviceAddress, regComID, commandID, wordsToWrite, &numWordsWritten, &status);
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    //Possivelmente na linha abaixo, devo fornecer o resultado da leitura a uma função de tratativa de erro
+    //Inserir parte do código para tratamento de erro... se resultado for OK sigo adiante, se for um erro processo o erro e saio da função.
+}
+
+/*  
+    Function to run a command
+    Return: none
+    Inputs: 
+        -
+        -
+*/
+void LeptonRun()
+{
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    DEV_I2C_MasterWriteData(1, deviceAddress, regComID, commandID, wordsToWrite, &numWordsWritten, &status);
+    //Possivelmente na linha abaixo, devo fornecer o resultado da leitura a uma função de tratativa de erro
+    //Read the STATUS register (register address 0x0002) bit 0
+    DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    while((readDataPtr & 0x0001) != 0x0000)
+    {
+        DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+    }
+    //Possivelmente na linha abaixo, devo fornecer o resultado da leitura a uma função de tratativa de erro
+    //Inserir parte do código para tratamento de erro... se resultado for OK sigo adiante, se for um erro processo o erro e saio da função.
+}
+
+/*  
+    Function to compile startup section
+    Return: none
+    Inputs: none
+*/
+void StartUpSequence()
+{
+	//power to be applied first --> HW
+	//clocks applied --> HW
+	//then RESET_L de-asserted --> HW (placa de desenvolvimento liga o pino diretamente ao Vcc)
+	//wait 5 seconds to allow the camera automatic execution of a flat-field correction (Auto FFC mode)
+	wait(5000);
+	//Read the STATUS register (register address 0x0002) bit 2
+	DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	while((readDataPtr & 0x0004) != 0x0000)
+	{
+		DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	}
+	//Read the STATUS register (register address 0x0002) bit 0
+	DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	while((readDataPtr & 0x0001) != 0x0000)
+	{
+		DEV_I2C_MasterReadData(1, deviceAddress, regStatus, readDataPtr, wordsToRead, &numWordsRead, &status);
+	}
+	//Poll the SYS FFC Status command (see 4.5.18), to verify the system is ready to process a new command (System Ready returned).
+	while(sysStatus == LEP_SYS_STATUS_BUSY)
+   {
+      LEP_GetSysFFCStatus(&_port,&sysStatus);
+   }
+}
+
+/*  
+    Function to open a comunication port
+    Return: result of open port command
+    Inputs: none
+*/
+LEP_RESULT lepton_connect() 
+{
+    //Possible clocks are: 100Kbaud, 400Kbaud and 1M baud
+	LEP_RESULT result = LEP_OpenPort(1, LEP_CCI_TWI, 400, &_port);
+	if (result != LEP_OK) printf("LEP_OpenPort(): %i\n", result);
+	_connected = true;
+	return result;
+}
+
+ 
+
+int main()
+{
+    StartUpSequence();
+    lepton_connect();
+	
+    //Enabling AGC preprocessing
+    LEP_SetAgcEnableState(&_port, LEP_AGC_ENABLE);
+    //Getting the AGC enabble status
+    LEP_AGC_ENABLE_E _isAGCEnabled;
+    LEP_RESULT result = LEP_GetAgcEnableState(&_port, &_isAGCEnabled);
+    //check to see if status is corrected set. Must just pass
+    while(result != LEP_OK);
+    //To inform error
+    //if (result != LEP_OK) printf("LEP_GetAgcEnableState(): %i\n", result);
+
+    //SYS FFC Mode Control
+    LEP_SYS_FFC_SHUTTER_MODE_OBJ_T shutterModeObjPtr;      //structure to get FFC parameters    
+    LEP_SYS_FFC_SHUTTER_MODE_OBJ_T shutterModeObj;         //como definir valores pra strutura ??
+    //
+    //
+    //Get sequence:
+    LEP_GetFfcShutterModeObj(&_port, shutterModeObjPtr);
+    //como ler valores da estrutura
+    //
+    //Set sequence:
+    LEP_SetFfcShutterModeObj(&_port, shutterModeObj);
+
+    //Selecting Policy
+    LEP_AGC_POLICY_E_PTR agcPolicyPtr
+    //LEP_AGC_POLICY_E agcPolicy;
+    //Setting up HEQ policy
+    LEP_SetAgcPolicy(&_port, LEP_AGC_HEQ);
+    result = LEP_GetAgcPolicy(&_port, &agcPolicyPtr);
+    while(result != LEP_OK);
+    //Setting up Linear policy
+    LEP_SetAgcPolicy(&_port, LEP_AGC_LINEAR);
+    result = LEP_GetAgcPolicy(&_port, &agcPolicyPtr);
+    while(result != LEP_OK);
+
+
+    //Button
+    //pin 10 uartRX  GPIO 15 // 5V: pin 2 // GND: pin 9
+    //need to change pull up to 3V3 pin
+    using namespace std;
+
+    wiringPiSetup();        // Setup the library
+    pinMode(15, INPUT);     // Configure GPIO15 as an input
+
+    if(digitalRead(15) == 1)
+    {
+        //capture
+    }
+}
